3addacd3d89b576325ac5c0e4c08e5072cb46c4b
Andrew Kaiser <kaisea.rpi@gmail.com>
2019-11-26 13:31:25 -0500

    Update test.js

--- test.js
+++ test.js
@@ -6,3 +6,8 @@ Hydra.get("account", async function(request) {
   const response = await Hydra.Client.get("/accounts/me")
   return response.body
 })
+
+Hydra.get("get_account", async function(request) {
+  const response = await Hydra.Client.get("/accounts/me")
+  return new SSCSuccess(0, response.body)
+})
