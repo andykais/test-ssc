40abea9c9ec5106b27c4526848533a54b8471831
Andrew Kaiser <kaisea.rpi@gmail.com>
2020-01-23 14:21:52 -0500

    add onload call

--- test.js
+++ test.js
@@ -1,3 +1,41 @@
+Hydra.onLoad(async function(response) {
+  Logger.error('Hydra.onLoad start');
+  const serverAuth = Hydra.Client.authServer();
+
+  await Hydra.Client.get('/health/check', { auth: serverAuth })
+  await Hydra.Client.get('/health/check', { auth: serverAuth })
+  await Hydra.Client.get('/health/check', { auth: serverAuth })
+  await Hydra.Client.get('/health/check', { auth: serverAuth })
+  await Hydra.Client.get('/health/check', { auth: serverAuth })
+  await Hydra.Client.get('/health/check', { auth: serverAuth })
+  await Hydra.Client.get('/health/check', { auth: serverAuth })
+
+  Logger.error('Hydra.onLoad complete');
+});
+
+Hydra.get('synchronous', function(request) {
+  Logger.error('synchronous endpoint start');
+  Logger.error('synchronous endpoint end');
+  return { synchonous: true };
+});
+
+Hydra.get('d_resolved', function(request) {
+  Logger.error('d_resolved endpoint start');
+  var speedy_return_value = request.userRequest.queryparams.TestInput;
+  Logger.error('d_resolved endpoint end');
+  return D.resolved(speedy_return_value);
+});
+
+Hydra.get('promise', function(request) {
+  return new Promise(resolve => {
+    Logger.error('fast_return endpoint start');
+    Hydra.Client.get('/health/check').then(() => {
+      Logger.error('promise endpoint resolve');
+      resolve(true);
+    });
+  });
+});
+
 Hydra.get("ping", function(request) {
   return "pong";
 })
